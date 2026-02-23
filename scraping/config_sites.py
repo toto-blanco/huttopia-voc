c1b49f9a1869691f2e9be96c2be83dbdfe2ab1e7
Huttopia Project <huttopia-project@example.com>
2026-02-23 21:07:32 +0100

    modification requirements.txt et config sites (ajouts sites booking)

---
+++ scraping/config_sites.py
@@ -0,0 +1,33 @@
+# scraping/config_sites.py
+
+CONFIG = {
+    "Huttopia": {
+        "trustpilot_url": "https://fr.trustpilot.com/review/huttopia.com",
+        "booking_urls": [
+            "https://www.booking.com/hotel/fr/huttopia-versailles.fr.html#tab-reviews",
+            "https://www.booking.com/hotel/fr/huttopia-dieulefit.fr.html#tab-reviews",
+            "https://www.booking.com/hotel/fr/huttopia-sarlat.fr.html#tab-reviews",
+            "https://www.booking.com/hotel/fr/huttopia-gorges-du-verdon.fr.html#tab-reviews",
+            "https://www.booking.com/hotel/fr/huttopia-le-moulin.fr.html#tab-reviews",
+            "https://www.booking.com/hotel/fr/huttopia-rambouillet.fr.html#tab-reviews",
+            "https://www.booking.com/hotel/fr/huttopia-arcachon.fr.html#tab-reviews",
+            "https://www.booking.com/hotel/fr/huttopia-font-romeu.fr.html#tab-reviews",
+            "https://www.booking.com/hotel/fr/village-huttopia-sud-ardeche.fr.html#tab-reviews",
+            "https://www.booking.com/hotel/fr/camping-le-lac-serre-poncon.fr.html#tab-reviews"
+        ],
+        "tripadvisor_urls": [
+            "https://www.tripadvisor.fr/Hotel_Review-g187148-d645632-Reviews-Camping_Huttopia_Versailles-Versailles_Yvelines_Ile_de_France.html",
+            "https://www.tripadvisor.fr/Hotel_Review-g1055982-d1109041-Reviews-Camping_Huttopia_Rambouillet-Rambouillet_Yvelines_Ile_de_France.html",
+            "https://www.tripadvisor.fr/Hotel_Review-g286341-d3451816-Reviews-Village_Huttopia_Dieulefit-Dieulefit_Drome_Auvergne_Rhone_Alpes.html",
+            "https://www.tripadvisor.fr/Hotel_Review-g196508-d4759846-Reviews-Camping_Huttopia_Sarlat-Sarlat_la_Caneda_Dordogne_Nouvelle_Aquitaine.html",
+            "https://www.tripadvisor.fr/Hotel_Review-g677532-d2643808-Reviews-Camping_Huttopia_Gorges_du_Verdon-Castellane_Alpes_de_Haute_Provence_Provence_Alpes_Co.html",
+            "https://www.tripadvisor.fr/Hotel_Review-g4206775-d4808574-Reviews-Camping_Huttopia_Le_Moulin-Saint_Julien_de_Peyrolas_Gard_Occitanie.html"
+
+        ]
+    },
+    "Sandaya": {
+        "trustpilot_url": "https://fr.trustpilot.com/review/sandaya.fr",
+        "booking_urls": [],
+        "tripadvisor_urls": []
+    }
+}
